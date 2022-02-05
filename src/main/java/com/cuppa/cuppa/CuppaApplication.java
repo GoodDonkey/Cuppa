c0b3714a9ce3198f7da7d202be1a7b5c8a84b0ce
GoodDonkey <president20500@gmail.com>
2022-02-05 16:34:38 +0900

    initial commit

---
+++ src/main/java/com/cuppa/cuppa/CuppaApplication.java
@@ -0,0 +1,13 @@
+package com.cuppa.cuppa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuppaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuppaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuppa/cuppa/CuppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuppa.cuppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
