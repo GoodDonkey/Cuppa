290ae6d44d99003ef2f82c84d4f9ed8ebc5b1441
GoodDonkey <president20500@gmail.com>
2022-03-18 18:09:31 +0900

    add: RabbitMQ config

--- src/main/java/com/cuppa/cuppa/messaging/configuration/WebsocketConfiguration.java
+++ src/main/java/com/cuppa/cuppa/messaging/configuration/WebsocketConfiguration.java
@@ -2,6 +2,7 @@ package com.cuppa.cuppa.messaging.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,7 +20,9 @@ public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher("."));
         registry.setApplicationDestinationPrefixes("/app"); // send 요청 처리
-        registry.enableSimpleBroker("/topic"); // 이 경로를 subscribe하는 client에게 메시지를 전달
+//        registry.enableSimpleBroker("/topic"); // 이 경로를 subscribe하는 client에게 메시지를 전달
+        registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue");
     }
 }
