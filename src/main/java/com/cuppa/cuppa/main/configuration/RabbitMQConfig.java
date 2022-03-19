043d8b68b909b151cfeab0762ce9773696ddadcd
GoodDonkey <president20500@gmail.com>
2022-03-19 16:27:13 +0900

    edit: todo and erased comments

--- src/main/java/com/cuppa/cuppa/main/configuration/RabbitMQConfig.java
+++ src/main/java/com/cuppa/cuppa/main/configuration/RabbitMQConfig.java
@@ -1,7 +1,6 @@
 package com.cuppa.cuppa.main.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.core.Binding;
@@ -13,7 +12,6 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -27,28 +25,25 @@ public class RabbitMQConfig {
     private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
     private static final String ROUTING_KEY = "to.*";
     
-    //Queue 등록
     @Bean
     public Queue queue(){ return new Queue(CHAT_QUEUE_NAME, true); }
     
-    //Exchange 등록
     @Bean
     public TopicExchange exchange(){ return new TopicExchange(CHAT_EXCHANGE_NAME); }
     
-    // Exchange 와 Queue 바인딩
     @Bean
     public Binding binding(Queue queue, TopicExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
     
-    // messageConverter를 커스터마이징 하기 위해 RabbitTemplate Bean 을 새로 등록
+    // messageConverter 를 커스터마이징 하기 위해 RabbitTemplate Bean 을 새로 등록
     // ConnectionFactory, MessageConverter 빈은 아래에서 등록하고 있다.
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
                                          MessageConverter jsonMessageConverter){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jsonMessageConverter);
-        rabbitTemplate.setRoutingKey(CHAT_QUEUE_NAME);
+        rabbitTemplate.setRoutingKey(ROUTING_KEY);
         return rabbitTemplate;
     }
     
@@ -73,16 +68,11 @@ public class RabbitMQConfig {
     
     @Bean
     public MessageConverter jsonMessageConverter(){
-        //LocalDateTime serializable을 위해
+        // Todo: 시간 정보를 넣는 로직을 처리하기 (대체해야함)
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true);
         objectMapper.registerModule(new JavaTimeModule());
     
         return new Jackson2JsonMessageConverter(objectMapper);
     }
-//
-//    @Bean
-//    public Module dateTimeModule(){
-//        return new JavaTimeModule();
-//    }
 }
