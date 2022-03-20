19fd7d18eadb047d7d9d98602f345ce4b09693b1
GoodDonkey <president20500@gmail.com>
2022-03-20 18:15:54 +0900

    add: Cors mapping for intellij local server
    프론트엔드 서버 띄울 때 intellij 로 띄우면 63342 포트를 사용함

--- src/main/java/com/cuppa/cuppa/main/configuration/WebConfig.java
+++ src/main/java/com/cuppa/cuppa/main/configuration/WebConfig.java
@@ -44,7 +44,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://127.0.0.1:5500/",
                                 "http://localhost:5500",
                                 "ws://127.0.0.1:5500/",
-                                "http://localhost:8080/")
+                                "http://localhost:8080/",
+                                "http://127.0.0.1:63342/")
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true).maxAge(3600);
     }
