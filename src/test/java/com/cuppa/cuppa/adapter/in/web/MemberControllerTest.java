313fcd041dd08b72c80e1a75066f371996d55132
GoodDonkey <president20500@gmail.com>
2022-03-29 20:33:34 +0900

    test: hex - member bean type check

---
+++ src/test/java/com/cuppa/cuppa/adapter/in/web/MemberControllerTest.java
@@ -0,0 +1,25 @@
+package com.cuppa.cuppa.adapter.in.web;
+
+import com.cuppa.cuppa.application.port.MemberFetchUseCase;
+import com.cuppa.cuppa.application.port.MemberSaveUseCase;
+import com.cuppa.cuppa.application.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = "spring.profiles.active:dev")
+public class MemberControllerTest {
+    @Autowired private MemberSaveUseCase memberSaveUseCase;
+    @Autowired private MemberFetchUseCase memberFetchUseCase;
+    
+    @Test
+    @DisplayName("Member UseCase 에 MemberService 가 주입되었는가?")
+    void checkUseCaseType() {
+        assertThat(memberSaveUseCase).isInstanceOf(MemberService.class);
+        assertThat(memberFetchUseCase).isInstanceOf(MemberService.class);
+    }
+    
+}
