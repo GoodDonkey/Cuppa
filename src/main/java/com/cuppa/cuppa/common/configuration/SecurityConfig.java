4d68f461d6e3924c1558e0923b20ae9adfbe94b0
GoodDonkey <president20500@gmail.com>
2022-04-13 13:19:49 +0900

    fix: ajax authentication exception
    비동기 요청에 대해서 html을 반환하는 문제를 401을 반환하도록 고침

--- src/main/java/com/cuppa/cuppa/common/configuration/SecurityConfig.java
+++ src/main/java/com/cuppa/cuppa/common/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.cuppa.cuppa.common.configuration;
 
 import com.cuppa.cuppa.application.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -22,58 +23,57 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        
+
 //        http.csrf().disable();
-        
-        http.cors();
-        
-        http.authorizeRequests()
-            .antMatchers("/", "/members/add", "/login", "/logout", "/images/**", "/css/**", "/*.ico","/v3/api-docs/**", "/null/**",
-                         "/chat/**", "/error", "/registration/**", "/topic/**", "/stomp/**", "/app/**", "/swagger-ui/**", "/openapi.json", "/swagger-ui.*",
-                         "/swagger-ui.html/**","/webjars/**", "/swagger-resources/**", "/META-INF/**", "/auth/**").permitAll()
-            .antMatchers("/admin").hasRole("ADMIN")
-            .anyRequest().authenticated();
-
-        http.formLogin()
-            .loginPage("/auth/login")
-            .loginProcessingUrl("/auth/login")
-            .defaultSuccessUrl("/", true)
+    
+        http.cors(); // WebConfig 에서 설정하면 CorsConfiguration 빈이 등록된다. 그 빈을 자동으로 사용함.
+    
+        http.authorizeRequests().antMatchers("/",
+                                             "/members/add",
+                                             "/login",
+                                             "/logout",
+                                             "/*.ico",
+                                             "/null/**",
+                                             "/chat/**",
+                                             "/error",
+                                             "/topic/**",
+                                             "/stomp/**",
+                                             "/app/**",
+                                             "/auth/**").permitAll()
+            .antMatchers("/admin").hasRole("ADMIN").anyRequest()
+            .authenticated();
+    
+        http.formLogin().loginPage("/auth/login").loginProcessingUrl("/auth/login").defaultSuccessUrl("/", true)
             .usernameParameter("loginId");
-        
+    
         // 인증에 실패할 경우 보낼 곳
-        http.exceptionHandling();
-        
-
-        http.logout()
-            .logoutUrl("/auth/logout")
-            .logoutSuccessUrl("/");
+        http.exceptionHandling()
+            .authenticationEntryPoint(new AjaxAuthenticationEntryPoint("/auth/login"));
+    
+    
+        http.logout().logoutUrl("/auth/logout").logoutSuccessUrl("/");
     
         http.userDetailsService(userDetailsService);
     }
     
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOriginPattern("*");
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.addAllowedHeader("*");
-//        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
-//        corsConfiguration.setMaxAge(3600L);
-//        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-//        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-//
-//        return urlBasedCorsConfigurationSource;
-//    }
     
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/v2/api-docs",
+                                   "/v3/api-docs/**",
                                    "/configuration/ui",
                                    "/swagger-resources/**",
-                                   "/configuration/security",
                                    "/swagger-ui.html",
+                                   "/swagger-ui.*",
+                                   "/swagger-ui/**",
+                                   "/swagger-ui.html/**",
+                                   "/swagger-resources/**",
+                                   "/openapi.json",
                                    "/webjars/**",
-                                   "/h2-console/**",
+                                   "/css/**",
+                                   "/images/**",
+                                   "/openapi.json",
+                                   "/META-INF/**",
                                    "/js/**");
     }
 }
