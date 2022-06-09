37466e544ce7227b8542142e48b30ff030ccce29
GoodDonkey <president20500@gmail.com>
2022-06-09 20:16:25 +0900

    test: Message Mapper test w/ mocks

--- src/test/java/com/cuppa/cuppa/adapter/in/web/dto/MessageMapperTest.java
+++ src/test/java/com/cuppa/cuppa/adapter/in/web/dto/MessageMapperTest.java
@@ -1,7 +1,9 @@
 package com.cuppa.cuppa.adapter.in.web.dto;
 
+import com.cuppa.cuppa.application.port.out.MemberFindByIdPort;
 import com.cuppa.cuppa.domain.Member;
 import com.cuppa.cuppa.domain.Message;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 // TODO: 스프링과 분리하여 테스트를 만들었는데, 스프링 위에서 돌리는 테스트도 필요할까?
 class MessageMapperTest {
     
-    private final MessageMapper messageMapper = new MessageMapper(new MemberMapper());
+    private MessageToMessageResponse messageToMessageResponse;
+    
+    @BeforeEach
+    void setting() {
+        this.messageToMessageResponse = new MessageDTOMapper(new MockMemberPersistenceAdapter(), new MockMemberDTOMapper());
+    }
     
     @Test
     void mapTest() {
@@ -23,7 +30,7 @@ class MessageMapperTest {
         Message message = Message.builder().message(m).receiver(receiver).sender(sender).build();
         
         // when
-        MessageDTO dto = messageMapper.map(message);
+        MessageResponseDTO dto = messageToMessageResponse.map(message);
         
         //then
         assertThat(dto.getMessage()).isEqualTo(message.getMessage());
@@ -40,15 +47,37 @@ class MessageMapperTest {
         Member receiver = Member.builder().username("receiverName").id(2L).password(password).build();
         
         Message message = Message.builder().message(m).receiver(receiver).sender(sender).build();
-        
+    
         // when
-        MessageDTO dto = messageMapper.map(message);
-        
+        MessageResponseDTO dto = messageToMessageResponse.map(message);
+    
         //then
         assertThat(dto.getSender()).isInstanceOf(MemberDTO.class);
         assertThat(dto.getReceiver()).isInstanceOf(MemberDTO.class);
-        
+    
         assertThat(dto.getSender()).isNotInstanceOf(Member.class);
         assertThat(dto.getReceiver()).isNotInstanceOf(Member.class);
+    }
+    
+    class MockMemberPersistenceAdapter implements MemberFindByIdPort {
+        
+        @Override
+        public Member findById(Long loginId) {
+            return Member.builder()
+                         .username("mockUser")
+                         .password("mockPassword")
+                         .build();
+        }
+    }
+    
+    class MockMemberDTOMapper implements MemberToMemberDTO {
+    
+        @Override
+        public MemberDTO map(Member member) {
+            MemberDTO memberDTO = new MemberDTO();
+            memberDTO.setId(member.getId());
+            memberDTO.setUsername(member.getUsername());
+            return memberDTO;
+        }
     }
 }
